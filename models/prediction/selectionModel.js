63cfbe6a15b540886755870aee1ccd2b5d59cd94
a Joshua Tree <133618057+Ayohinliveaid@users.noreply.github.com>
2025-03-23 16:31:11 +0800

    ARIMA模型

--- models/prediction/selectionModel.js
+++ models/prediction/selectionModel.js
@@ -4,6 +4,7 @@ const predictionModel = require("./predictionModel");
 const evaluationModel = require("./evaluationModel");
 
 //首先调用测试模型中所有预测方法，生成相应的拟合数据，具体来说，是多项式回归的方法中，使用不同的方法作为项数
+const degrees = [...Array(20)].map((v, i) => i + 1);
 
 //预测方法对象，包括预测函数，原数据，生成的拟合数据，生成一个预测方法对象的数组，来保存
 
